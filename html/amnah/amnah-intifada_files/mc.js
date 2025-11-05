47730b5b9e598d9a2e32e0d6a10212b63041e096
amad <amad@khajistan.com>
2025-11-05 18:10:45 -0500

    first commit

---
+++ html/amnah/amnah-intifada_files/mc.js
@@ -0,0 +1,45 @@
+var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
+if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
+{
+  let window = _____WB$wombat$assign$function_____("window");
+  let self = _____WB$wombat$assign$function_____("self");
+  let document = _____WB$wombat$assign$function_____("document");
+  let location = _____WB$wombat$assign$function_____("location");
+  let top = _____WB$wombat$assign$function_____("top");
+  let parent = _____WB$wombat$assign$function_____("parent");
+  let frames = _____WB$wombat$assign$function_____("frames");
+  let opener = _____WB$wombat$assign$function_____("opener");
+
+var ap = window.navigator.appVersion.toLowerCase();
+var i = ap.indexOf("msie");
+if (ap.indexOf("windows") >= 0 && i >= 0 && ((i + 5) < ap.length)) {
+  var v = ap.charAt(i + 5);
+  if (v >= 5 ) {
+    document.write('<scr' + 'ipt src="https://web.archive.org/web/20020402200934/http://us.i1.yimg.com/us.yimg.com/i/mc/mc1.js"></scr' + 'ipt>');
+    document.write('<scr' + 'ipt src="https://web.archive.org/web/20020402200934/http://us.i1.yimg.com/us.yimg.com/i/mc/mc2.js"></scr' + 'ipt>');
+  }
+}
+
+
+
+}
+/*
+     FILE ARCHIVED ON 20:09:34 Apr 02, 2002 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 06:04:53 Jul 15, 2025.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  captures_list: 0.619
+  exclusion.robots: 0.019
+  exclusion.robots.policy: 0.009
+  esindex: 0.009
+  cdx.remote: 55.379
+  LoadShardBlock: 424.793 (6)
+  PetaboxLoader3.datanode: 276.895 (7)
+  PetaboxLoader3.resolve: 185.483 (2)
+  load_resource: 159.088
+*/
---
+++ html/barghouti/barghouti-poem-folk_files/none.js
@@ -0,0 +1,68 @@
+var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
+if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
+{
+  let window = _____WB$wombat$assign$function_____("window");
+  let self = _____WB$wombat$assign$function_____("self");
+  let document = _____WB$wombat$assign$function_____("document");
+  let location = _____WB$wombat$assign$function_____("location");
+  let top = _____WB$wombat$assign$function_____("top");
+  let parent = _____WB$wombat$assign$function_____("parent");
+  let frames = _____WB$wombat$assign$function_____("frames");
+  let opener = _____WB$wombat$assign$function_____("opener");
+
+<!--hide script from old browsers
+//No Click! - version 0.6 - Main programme
+//Copyright © 1997 - 1999 by Enigma ® Designs and Kallback Africa. All rights reserved
+//Simply -  http://www.kallbackafrica.com/simply
+//mirror at http://www.kallback.co.za/simply
+//Programmed by Michel van Baalen - michel@kallback.co.za
+//This script may not be used without permission
+//
+// DO NOT change ANY of the following code !!!
+//
+BroW = parseInt(navigator.appVersion)
+if (BroW >= 4) {Vs = true} else {Vs = false}
+if (Vs == true) {
+  Wid = screen.availWidth - 30
+  Hei = screen.availHeight + 50
+  Left = Math.floor((Wid - 350) / 2) ; Top = Math.floor((Hei - 100) / 2)
+  if (document.layers) {
+    document.captureEvents(Event.MOUSEDOWN)
+  }
+  document.onmousedown = noClick
+}
+//
+function noClick (e) {
+  flag = false
+  if (document.layers) {which = e.which ; if (which == 3) {flag = true}
+  } else {which = event.button ; if (which == 2) {flag = true}}
+  if (flag) {
+    alert('Sorry, but the copying of images from this site is forbidden.\nPlease obtain permission from the webmaster to use\nour images!')
+    return false
+  }
+}
+//
+//-- end hiding --> 
+
+
+}
+/*
+     FILE ARCHIVED ON 15:15:44 Oct 21, 2001 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 03:19:51 Jul 15, 2025.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  captures_list: 0.538
+  exclusion.robots: 0.018
+  exclusion.robots.policy: 0.008
+  esindex: 0.01
+  cdx.remote: 6.836
+  LoadShardBlock: 47.257 (3)
+  PetaboxLoader3.datanode: 70.833 (4)
+  load_resource: 98.708
+  PetaboxLoader3.resolve: 53.073
+*/
